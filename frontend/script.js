bd759899c60f0baa18cda0ae32484d4136b17f27
Pranav770 <175470329+Pranav770@users.noreply.github.com>
2025-09-28 21:55:04 +0530

    Update the frontend
    add a new Your Task feature in the right

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+
+        const taskInput = document.getElementById("taskInput");
+        const addBtn = document.getElementById("addBtn");
+        const taskList = document.getElementById("taskList");
+
+        addBtn.addEventListener("click", () => {
+            const task = taskInput.value.trim();
+            if(task !== ""){
+                const li = document.createElement("li");
+                li.textContent = task;
+                taskList.appendChild(li);
+                taskInput.value = "";
+            }
+        });
+    
